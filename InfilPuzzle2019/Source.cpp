0b2b046b43192de378005ecd44f4fc4f9610a241
RogerVDW_VS2019 <rogerscode@gmail.com>
2019-12-29 10:11:48 +0100

    memory leak gefixt

--- InfilPuzzle2019/Source.cpp
+++ InfilPuzzle2019/Source.cpp
@@ -33,6 +33,8 @@ void Infi1()
 		}
 		else break;
 	}
+
+	// Resultaat tonen
 	if (nSprongen == sprongen.size()) std::cout << "success, nSprongen = " << nSprongen; else std::cout << "Fail na sprong: " << nSprongen << ", de kerstman valt dus bij sprong " << nSprongen + 1;
 	std::cout << "\nDruk op toets..";
 	std::cin.get();
@@ -63,15 +65,18 @@ void VulBoom(Node* node, std::vector<std::pair<int, int>>& flats, std::vector < 
 	{
 		if (kanVooruitLanden(node->pos, s, flats, hoogtes))
 		{
+			// creeer nieuwe landingspositie
 			std::pair<int, int> nextpos;
 			nextpos.first = node->pos.first + s.first + 1;
 			nextpos.second = hoogtes.find(nextpos.first)->second;
 			int energy = s.first + s.second;
 
+			// voeg toe aan de vector van mogelijke nieuwe posities van deze node, tenzij de positie al bestaat
+			// als de positie al bestaat, voeg alleen toe als benodigde energie voor de sprong lager is
+			// (die laatste check is eigenlijk niet nodig, want de basisSprongen zijn gesorteerd van klein naar groot)
 			if (node->nextFlats.size() == 0)
 			{
 				node->nextFlats.push_back(new Node({ nextpos,s, {}, node->totalEnergyUsed + energy }));
-				//std::cout << "Stap (virgin): " << s.first << "," << s.second << std::endl;
 			}
 			else
 			{
@@ -85,8 +90,6 @@ void VulBoom(Node* node, std::vector<std::pair<int, int>>& flats, std::vector < 
 						{
 							delete node->nextFlats[i];
 							node->nextFlats[i] = new Node({ nextpos,s, {}, node->totalEnergyUsed + energy });
-							//std::cout << "Stap (added): " << s.first << "," << s.second << std::endl;
-
 						}
 					}
 				}
@@ -97,6 +100,7 @@ void VulBoom(Node* node, std::vector<std::pair<int, int>>& flats, std::vector < 
 			}
 		}
 	}
+	// Pas recursie toe: vul de sub-trees (alle children van deze node)
 	for (auto p : node->nextFlats)
 	{
 		VulBoom(p, flats, basisSprongen, hoogtes);
@@ -105,10 +109,10 @@ void VulBoom(Node* node, std::vector<std::pair<int, int>>& flats, std::vector < 
 }
 void TraverseTree(Node* node, std::pair<int, int>& endpoint, std::set<int>& res)
 {
-	// Recursive function to reach all leafs
+	// Recursive function to reach all leafs, depth first
 	if (node->pos.first == endpoint.first && node->pos.second == endpoint.second)
 	{
-		// FUNCTION CALL TO APPLY AT LEAFS
+		// Function call to apply at leafs
 		int E = node->totalEnergyUsed;
 		res.insert(E);
 	}
@@ -121,14 +125,23 @@ void TraverseTree(Node* node, std::pair<int, int>& endpoint, std::set<int>& res)
 	}
 	return;
 }
-
+void Delete(Node* node, int& n)
+{
+	std::vector<Node*> children = node->nextFlats;
+	delete node;
+	node = nullptr;
+	n++;
+	for (auto c : children)
+	{
+		Delete(c,n);
+	}
+}
 void Infi2()
 {
 	// aoc.infi.nl 2019, deel 2
-	// Input
+	// Input (kan json load doen, maar dit was even sneller)
 	std::vector<std::pair<int, int>> flats = { {3,3},{5,5},{9,6},{13,7},{15,9},{16,10},{18,9},{21,10},{23,3},{25,4},{28,5},{29,7},{30,8},{34,7},{37,9},{39,7},{41,8},{42,10},{43,9},{47,10},{48,10},{50,6},{52,7},{53,10},{56,6},{57,4},{58,6},{59,2},{62,2} };
 	std::vector < std::pair<int, int>> basisSprongen = { {0,0},{0,1},{0,2 },{0,3},{0,4},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{3,0},{3,1},{4,0} };
-
 
 	// Maak map van alle hoogtes van de flats: KEY= positie x van flat, VALUE = hoogte
 	std::map<int, int> hoogtes;
@@ -149,6 +162,10 @@ void Infi2()
 	std::cout << "\nDruk op toets..";
 	std::cin.get();
 
+	// Ruim geheugen op (old school garbage collection, of ik moet smart pointers (c++14) gebruiken
+	int n = 0;
+	Delete(root, n); std::cout << n << " nodes deleted. End.";
+	std::cin.get();
 }
 
 int main()
@@ -157,13 +174,15 @@ int main()
 	// Deze oplossing maakt gebruik van een Tree met alle mogelijke routes die succesvol eindigen. 
 	// De nodes bevatten relevante data, waaronder de energie die nodig was om op de node te komen.
 	// De leaves van de Tree bevatten dus de energie tot het eindpunt van die unieke route.
-
+	// Niet efficient, volgens mij O(2^N).
+	//
 	// Code kan nog veel simpeler, maar simpel = moeilijk en tijd: geen 
 	// Ik wilde nog een mooie visualisatie van de tree maken, maar tijd: geen
 	// groeten! Rogier
 	//
 	// n.b.: heb net Graphs / grafen geleerd en een eigen OOP implementatie geschreven. 
-	// Realiseer me nu dat deze puzzel een schoolvoorbeeld is van een shortest path (optimaliseren voor energie)
+	// Realiseer me nu dat deze puzzel een schoolvoorbeeld is van een shortest path (optimaliseren voor energie).
+	// Dan hoef je dus niet meer alle mogelijke routes te verkennen.
 	// Ga nu Dijkstra's algo leren, stuur wel een update als ik deze puzzel daarmee heb getackeld. 
 	// Fantastiche leerervaring dus!!
 
